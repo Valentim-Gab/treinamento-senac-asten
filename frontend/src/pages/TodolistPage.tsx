ff7dd109d628d744012973bb6f1119eebbbf36aa
gabriel.arruda <gabriel.arruda@avmb-asten.com.br>
2026-06-19 09:05:27 -0300

    Adicionar operações de CRUDde todolist

--- frontend/src/pages/TodolistPage.tsx
+++ frontend/src/pages/TodolistPage.tsx
@@ -1,10 +1,16 @@
 import type { Todo } from '@/interfaces/Todo'
-import { findTodos } from '@/services/TodoService'
+import {
+  createTodo,
+  deleteTodo,
+  findTodos,
+  updateTodoCompleted,
+} from '@/services/TodoService'
 import { useEffect, useState } from 'react'
 import './todolist.css'
 
 export default function TodolistPage() {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [newTodo, setNewTodo] = useState('')
 
   useEffect(() => {
     void findTodos()
@@ -16,13 +22,78 @@ export default function TodolistPage() {
       })
   }, [])
 
+  const handleAdd = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    if (!newTodo.trim()) {
+      return
+    }
+
+    try {
+      const todo = await createTodo({ title: newTodo })
+
+      setTodos((prev) => [...prev, todo])
+      setNewTodo('')
+    } catch {
+      alert('Error creating todo')
+    }
+  }
+
+  const handleCompleted = async (id: number, completed: boolean) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: completed } : todo,
+      ),
+    )
+
+    try {
+      await updateTodoCompleted(id, completed)
+    } catch {
+      alert('Error updating todo')
+
+      setTodos((prev) =>
+        prev.map((todo) =>
+          todo.id === id ? { ...todo, completed: !completed } : todo,
+        ),
+      )
+    }
+  }
+
+  const handleDelete = async (id: number) => {
+    try {
+      await deleteTodo(id)
+      setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    } catch {
+      alert('Error deleting todo')
+    }
+  }
+
   return (
     <main className="todo-container">
+      <h1 className="todo-title">Todolist</h1>
+
+      <form className="todo-input-container" onSubmit={handleAdd}>
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Digite uma tarefa..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="todo-button-add" type="submit">
+          Adicionar
+        </button>
+      </form>
+
       <ul className="todo-list">
         {todos.length > 0 &&
           todos.map((todo) => (
             <li key={todo.id} className="todo-item">
-              <input type="checkbox" checked={todo.completed} readOnly />
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => handleCompleted(todo.id, e.target.checked)}
+              />
               <span
                 className="todo-text"
                 style={{
@@ -33,7 +104,12 @@ export default function TodolistPage() {
                 {todo.title}
               </span>
               <button className="todo-button-edit">Editar</button>
-              <button className="todo-button-delete">Excluir</button>
+              <button
+                className="todo-button-delete"
+                onClick={() => handleDelete(todo.id)}
+              >
+                Excluir
+              </button>
             </li>
           ))}
       </ul>
--- frontend/src/services/TodoService.ts
+++ frontend/src/services/TodoService.ts
@@ -1,10 +1,10 @@
 import { env } from '@/envs/Environment'
-import type { Todo } from '@/interfaces/Todo'
+import type { CreateTodo, Todo } from '@/interfaces/Todo'
+import { getToken } from './AuthService'
 
 export async function findTodos(): Promise<Todo[]> {
   try {
-    const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjMsImVtYWlsIjoiYWRtaW5AZW1haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzgxNjU3MjIyLCJleHAiOjE3ODE2NjA4MjJ9.jIAhJRDPClpgtQYcHd3iSe-XG7KCc2iM0MuyxFV6wos'
+    const token = getToken()
     const res = await fetch(`${env.apiBaseUrl}/todos`, {
       method: 'GET',
       headers: {
@@ -16,6 +16,63 @@ export async function findTodos(): Promise<Todo[]> {
     return await res.json()
   } catch (error) {
     console.error('Error fetching todos:', error)
+    throw error
+  }
+}
+
+export async function createTodo({ title }: CreateTodo): Promise<Todo> {
+  try {
+    const token = getToken()
+    const res = await fetch(`${env.apiBaseUrl}/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ title }),
+    })
+
+    return await res.json()
+  } catch (error) {
+    console.error('Error creating todo:', error)
+    throw error
+  }
+}
+
+export async function updateTodoCompleted(
+  id: number,
+  completed: boolean,
+): Promise<Todo> {
+  try {
+    const token = getToken()
+    const res = await fetch(`${env.apiBaseUrl}/todos/${id}/completed`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ completed }),
+    })
+
+    return await res.json()
+  } catch (error) {
+    console.error('Error updating todo completed:', error)
+    throw error
+  }
+}
+
+export async function deleteTodo(id: number): Promise<void> {
+  try {
+    const token = getToken()
+    await fetch(`${env.apiBaseUrl}/todos/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  } catch (error) {
+    console.error('Error deleting todo:', error)
     throw error
   }
 }
